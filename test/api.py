afd468288dbfef62abe231e1f1c4f819e75992db
Jang Wonyeong <wintery39@naver.com>
2025-05-26 00:44:35 +0900

    fix: reorganize folder structure

---
+++ test/api.py
@@ -0,0 +1,52 @@
+from cragmm_search.search import UnifiedSearchPipeline
+
+# initiate image search API only, web search API is not enabled for Task 1
+search_pipeline = UnifiedSearchPipeline(
+    image_model_name="openai/clip-vit-large-patch14-336",
+    image_hf_dataset_id="crag-mm-2025/image-search-index-validation",
+)
+
+
+import requests
+from PIL import Image
+
+import time
+
+image_li = []
+
+image_url = "image.png"
+origin_image = Image.open(image_url)
+# image = ImageLoader(image_url).get_image()
+image_li.append(origin_image)
+
+width, height = origin_image.size
+
+# 중간 지점
+mid_x = width // 2
+mid_y = height // 2
+
+# 각 영역 자르기 (crop의 인자는 (left, upper, right, lower))
+divide_coordinates = [
+    (0, 0, mid_x, mid_y),  # 왼쪽 위
+    (mid_x, 0, width, mid_y),  # 오른쪽 위
+    (0, mid_y, mid_x, height),  # 왼쪽 아래
+    (mid_x, mid_y, width, height)  # 오른쪽 아래
+]
+
+for x, y, w, h in divide_coordinates:
+    cropped_image = origin_image.crop((x, y, w, h))
+    image_li.append(cropped_image)
+
+results = []
+start_time = time.time()
+print("start time: "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+for image in image_li:
+  response = search_pipeline(image, k = 5)
+  assert response is not None, "No results found"
+  results.extend(response)
+print("end time: "+time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
+print(f"Time taken for search: {time.time() - start_time:.2f} seconds")
+
+for result in results:
+    print(result)
+    print('\n')
